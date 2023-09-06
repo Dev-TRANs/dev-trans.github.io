6d54eaaf5686ff990f9a6e75b4229111780ecff2
Shotaro Nakamura <syoutarou1103@outlook.jp>
2023-09-06 08:58:11 +0000

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), mdx()]
+  integrations: [tailwind(), preact(), mdx(), sitemap()]
 });
