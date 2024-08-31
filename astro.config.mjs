9f7bac902b51a042a6f0c7ba8252dd3b13c3198c
-akku- <151517253+akku1139@users.noreply.github.com>
2024-09-01 00:01:53 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), preact(), sitemap()],
   site: "https://trans.stki.org",
 });
